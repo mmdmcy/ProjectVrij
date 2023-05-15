78e7ecdd9e1c5989e8bb6bcfdce805f52a0d9b55
JTH <112551618+mmdmcy@users.noreply.github.com>
2023-05-15 21:52:05 +0200

    Prototype V1

---
+++ ProjectVrij/Assets/Door.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject door_closed, door_opened, intText;
+    public AudioSource open, close;
+    public bool opened;
+
+    void OnTriggerStay(Collider other) {
+        if(other.CompareTag("MainCamera")) {
+            intText.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.E)) {
+                door_closed.SetActive(false);
+                door_opened.SetActive(true);
+                intText.SetActive(false);
+                //open.Play();
+                opened = true;
+            }
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        if(other.CompareTag("MainCamera")) {
+            intText.SetActive(false);
+        }
+    }
+    IEnumerator repeat() {
+        yield return new WaitForSeconds(4.0f);
+        opened = false;
+        door_closed.SetActive(true);
+        door_opened.SetActive(false);
+    }
+}
